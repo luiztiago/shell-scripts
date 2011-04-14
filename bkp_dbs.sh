3d70398f3b2df14fff1eff51408587041fb0b5f4
luiztiago <luiztiago@gmail.com>
2011-04-14 18:00:51 -0300

    backup com folder

--- bkp_dbs.sh
+++ bkp_dbs.sh
@@ -1,5 +1,8 @@
+folder=`date "+%Y-%m-%d_%H-%M-%S"`;
 query=$(mysql -f -u root -e 'show databases' -s --skip-column-names);
+mkdir $folder
+cd $folder
 
-for I in $query; do 
-		mysqldump -u root $I | gzip > "$I.sql.gz"
+for I in $query; do
+                mysqldump -u root $I | gzip > "$I.sql.gz"
 done
